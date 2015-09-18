00378f0a00d5fadda50e077fba3fb22fddf51c06
sarafina527 <1605010356@qq.com>
2015-09-18 19:25:55 +0800

    1

---
+++ Weather/src/WeatherData.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class WeatherData implements Subject {
+	
+	private ArrayList observers;
+	private float temperature;
+	
+	
+
+	@Override
+	public void registerObserver(Observer o) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeObserver(Observer o) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void notifyObservers() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
